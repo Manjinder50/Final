426ac7eb4ee093fbcbcd0704dd2696cbd3d66f69
Manjinder50 <manjinderbharaj50@gmail.com>
2021-07-17 00:41:42 +0530

    Added Login and Register forms without logic and removed a warning.

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 import logo from '../../assets/logo.svg'
 import Button from "@material-ui/core/Button";
 import Modal,{setAppElement} from "react-modal";
-import {Box, makeStyles, Tab, Tabs} from "@material-ui/core";
+import {Box, makeStyles, Tab, Tabs, FormControl,TextField} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 
@@ -20,7 +20,7 @@ function TabPanel(props) {
         >
             {value === index && (
                 <Box p={3}>
-                    <Typography>{children}</Typography>
+                    <Typography component={'div'}>{children}</Typography>
                 </Box>
             )}
         </div>
@@ -65,6 +65,8 @@ const Header = () => {
     useEffect(()=>{
         Modal.setAppElement('body');
     })
+
+    document.querySelectorAll(" p * div ")
     return(
     <header>
         <div>
@@ -78,10 +80,42 @@ const Header = () => {
                         <Tab label="REGISTER" {...a11yProps(1)} />
                     </Tabs>
             <TabPanel value={value} index={0}>
-                Item One
+                <FormControl>
+                    <TextField id="username" label="Username *" color="secondary" />
+                    <br/>
+                    <TextField
+                        id="password"
+                        label="Password *"
+                        color="secondary"
+                    />
+                    <br/><br/>
+                    <div className={"MuiButton-label"}>
+                    <Button variant="contained" color={"primary"}>Login</Button>
+                    </div>
+                </FormControl>
             </TabPanel>
             <TabPanel value={value} index={1}>
-                Item Two
+                <FormControl>
+                    <TextField id="firstName" label="First Name *" color="secondary" />
+                    <br/>
+                    <TextField
+                        id="lastName" label="Last Name*" color="secondary"
+                    />
+                    <br/>
+                    <TextField id="email" label="Email *" color="secondary" />
+                    <br/>
+                    <TextField
+                        id="password" label="Password *" color="secondary"
+                    />
+                    <br/>
+                    <TextField
+                        id="contactNo" label="Contact No. *" color="secondary"
+                    />
+                    <br/><br/>
+                    <div className={"MuiButton-label"}>
+                        <Button variant="contained" color={"primary"}>Register</Button>
+                    </div>
+                </FormControl>
             </TabPanel>
                 </Modal>
         </div>
