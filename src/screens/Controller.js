1d604d5d8c24fb466fba305b42a54e882427ef19
Manjinder50 <manjinderbharaj50@gmail.com>
2021-07-18 01:01:31 +0530

    Added Home.css and Home.js and Upcoming movie Header

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,12 +1,18 @@
 import Header from "../common/header/Header";
-import React from "react";
+import React,{Fragment} from "react";
+import Home from "./home/Home";
 
 export default function Controller(){
 
     return (
+        <Fragment>
         <div>
             <Header heading="Final" />
             <div className={"header"}></div>
         </div>
+        <div>
+            <Home />
+        </div>
+        </Fragment>
     );
 }
